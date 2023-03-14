4add588ba55f2b9327ae1e3c9af58c02c3c732e5
jonnathancuvi2000 <jonnathancuvi2000@gmail.com>
2023-03-14 16:03:51 -0500

    portafolio-next

--- pages/property.jsx
+++ pages/property.jsx
@@ -8,13 +8,17 @@ import { useRouter } from 'next/router'
 const Property = () => {
   const router = useRouter();
   const data = router.query;
+  console.log(data.img)
+
   return (
     <div className='w-full'>
 
       <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
-      {/* before it was this "bg-black/80" I changed to bg-black/60 */}
+        {/* before it was this "bg-black/80" I changed to bg-black/60 */}
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/60 z-10' />
-        <Image className='absolute z-1' layout='fill' objectFit='cover' src={data.img ? '/../public/assets/projects/' + data.img : propertyImg} alt='/' />
+        {/* {require(`./assets/images/dynamic/image-${imageId}.jpeg`)} */}
+        {/* '/../public/assets/projects/' + data.img  */}
+        <Image className='absolute z-1' layout='fill' objectFit='cover' src={data.img ? require(`../public/assets/projects/${data.img}`) : propertyImg} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
           <h2 className='py-2'>Propiedades</h2>
           <h3>
